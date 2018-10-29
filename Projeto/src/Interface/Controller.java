26763fcfd4cbfc84dd7c184abd668a6f7ffb047c
jdsfa1-iscteiul <jdsfa1@iscte-iul.pt>
2018-10-29 20:27:12 +0000

    Os posts do facebook aparecem na interface.
    Os posts são carregados para um txt onde ficam guardados e são
    posteriormente lidos para a interface.

--- Projeto/src/Interface/Controller.java
+++ Projeto/src/Interface/Controller.java
@@ -3,7 +3,12 @@ package Interface;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
@@ -24,13 +29,32 @@ public class Controller {
 		System.out.println("Searching" + " for "+getBoxText() );
 	}
 	
-	public void handleHomeButton() {
-		text_posts.setText("Ola, encontrámos estas mensagens nos teus grupos: \n");
+	public void handleHomeButton() throws InterruptedException {
 		FacebookWorkingThread fbw = new FacebookWorkingThread(text_posts);
 		fbw.start();
+		fbw.join();
+		text_posts.setText("Ola, encontrámos estas mensagens nos teus grupos: \n");
+		readDataAndWriteOnGui();
 	}
 	
 	private String getBoxText() {
 		return text_box.getText();
 	}
+	
+	 public void readDataAndWriteOnGui() {
+	        try {
+	        	Scanner file = new Scanner (new File("C:\\Users\\jonic\\OneDrive - ISCTE-IUL\\FACULDADE\\ANO_03_SEMESTRE_1\\ES1-Repositorio\\ES1-2018-LEI-PL-100\\Projeto\\src\\Interface\\posts_database.txt"));
+	 
+	            String line;
+	 
+	            while (file.hasNextLine()) {
+	            	line = file.nextLine();
+	            	text_posts.appendText("\n" + line+"\n");
+	            }
+	            file.close();
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
--- Projeto/src/Interface/FacebookWorkingThread.java
+++ Projeto/src/Interface/FacebookWorkingThread.java
@@ -1,5 +1,13 @@
 package Interface;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,40 +24,68 @@ public class FacebookWorkingThread extends Thread {
 	
 	private String idGrupoIscte = "";
 	private TextArea tx;
+	private PrintWriter fileWriter;
 	
 	public FacebookWorkingThread(TextArea tx) {
 		this.tx = tx;
 	}
 	
 	public void start() {
-		String at = "EAAdoDZCjMNoEBABEtZCdVZC8yMsOZAyBOl2VzJHruDxn4IIxw1sDxvtFgneEzNSwBH1q9dk6qS1UBDfZCbZCN7ZBjqqv2ZArbp1u4baADCdl64pZBrrSsFaNYRqAT85PnOOCoDZBsNZCqPotx4zO6E9IfMb6G1NlYYDo1EZD";
-
+		try {
+			//this.fileWriter = new PrintWriter (new File ("C:\\Users\\jonic\\OneDrive - ISCTE-IUL\\FACULDADE\\ANO_03_SEMESTRE_1\\ES1-Repositorio\\ES1-2018-LEI-PL-100\\Projeto\\src\\Interface\\posts_database.txt"));
+			BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\jonic\\OneDrive - ISCTE-IUL\\FACULDADE\\ANO_03_SEMESTRE_1\\ES1-Repositorio\\ES1-2018-LEI-PL-100\\Projeto\\src\\Interface\\posts_database.txt"));
+//			writer.write("teste");
+//			writer.close();
+//		} catch (final FileNotFoundException e)	{
+//			System.out.println("Erro a criar o filewriter");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}	
+//		
+		
+		//fileWriter.println("Ola, este teste funcionou");
+		String at = "EAAFBpVUIgNIBAFCvP5salaEZA2SdtFPkLxJykkiyHHzLSQgZCk3cl3fG4AzerwjM1mY6bXcnxVzxfavR8KhaKYZCkZB5aMRyXgSMAnelct7v7wzAdGcjVbC1CLZC9ZA3eZCUPUXm4PIPBAeZCbpKgmhbsPBmJLYphPdpm7kKzm2Y5EEfVsnkBNuy";
 		@SuppressWarnings("deprecation")
 		FacebookClient fb = new DefaultFacebookClient(at);
 
 		User me = fb.fetchObject("me", User.class);
 		System.out.println("Utilizador: \n" + me.getName() +"\n");
+		//tx.appendText(me.getName());
 
 
 		Connection<Group> groups = fb.fetchConnection("me/groups", Group.class);
 
 		for(List<Group> groupPages: groups) {
 			for(Group g: groupPages) {
-				if (g.getName().equals("TURMA LEI PL")) {
+				if (g.getName().equals("LEI PL ES1")) {
 					idGrupoIscte = g.getId();
 					System.out.println("Grupo: " + g.getName() + "\n");
+					
 				}
 			}
 		}
-
+		
 		Connection<Post> posts = fb.fetchConnection(idGrupoIscte+"/feed", Post.class);
 		
 		for (List<Post> postPages: posts) {
 			for(Post post: postPages) {
-				tx.appendText(post.getMessage());
+				System.out.println(post.getMessage());
+					//fileWriter.write(post.getMessage());
+				if(post.getMessage()!=null){
+					writer.append(post.getMessage()+"\n");
+				}	
 			}
-					
 		}	
+		
+		writer.close();
+		} catch (final FileNotFoundException e)	{
+			System.out.println("Erro a criar o filewriter");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
 	}
 
 }
